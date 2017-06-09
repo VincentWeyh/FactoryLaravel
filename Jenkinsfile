20480eb171b03d5324687fb86ecaf67bbabaa36e
VincentWeyh <vincent.weyh@live.fr>
2017-06-09 10:10:36 +0200

    try trigger

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,5 +1,8 @@
 pipeline {
   agent any
+  triggers{
+    poolSCM('* * * * *')
+  }
   stages {
     stage('Build') {
       steps {
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    New laravel
+                    NEW NEW LARAVEL
                 </div>
 
                 <div class="links">
