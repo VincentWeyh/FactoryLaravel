5b367752449595c15cc81ac6500443d58da9a8ce
VincentWeyh <vincent.weyh@live.fr>
2017-06-09 16:06:22 +0200

    test new title boop

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ioioioioioioio
+                    BOOP !
                 </div>
 
                 <div class="links">
