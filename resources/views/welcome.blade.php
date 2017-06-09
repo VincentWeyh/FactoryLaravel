62a1528db24b8d36be62f4777d6ba25f80fab8a4
VincentWeyh <vincent.weyh@live.fr>
2017-06-09 12:10:17 +0200

    change titlee

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    WOOOOOOOOOOOOOOOOOOOOOOOOOOOT
+                    LUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUULLULULUL
                 </div>
 
                 <div class="links">
