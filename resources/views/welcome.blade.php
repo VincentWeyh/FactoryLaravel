233d68bc3adbc7f8b6025fc87a7cbc3f357b1416
VincentWeyh <vincent.weyh@live.fr>
2017-06-09 14:55:22 +0200

    test new title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOoo
+                    uuuuuuuuuuuuuuuuuuuuuuuuuu
                 </div>
 
                 <div class="links">
