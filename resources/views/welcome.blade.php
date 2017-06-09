ac89ede7d6ca8c8eb05b44312d7401d94aa5be8a
VincentWeyh <vincent.weyh@live.fr>
2017-06-09 09:37:29 +0200

    A new push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    New laravel
                 </div>
 
                 <div class="links">
