03967f6151a0d2f7f7de7286948d7c745483ee32
VincentWeyh <vincent.weyh@live.fr>
2017-06-09 15:05:50 +0200

    test new tite ioioioio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    iiiiiiiiiiiiiiiiiiiiiiiiiii
+                    ioioioioioioio
                 </div>
 
                 <div class="links">
