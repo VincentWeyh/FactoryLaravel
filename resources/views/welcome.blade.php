80ce02e009c5a441457bbda183ffe7b5edd3b209
VincentWeyh <vincent.weyh@live.fr>
2017-06-09 10:22:56 +0200

    change title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    NEW NEW LARAVEL
+                    TEST 123
                 </div>
 
                 <div class="links">
